7a05823c3e2f931fb25ee9fd26990bf1094d495e
admin <patromi123@gmail.com>
2023-10-18 19:03:37 +0200

    fixing bugs

--- python/currency-converter/converter.py
+++ python/currency-converter/converter.py
@@ -1,6 +1,10 @@
 from converter.App import App
 import sys
+import os
 
 
+if os.getenv('ACCESS_KEY') is None:
+    print("Please set your AccessKey environment variable")
+    sys.exit(1)
 app = App(sys.argv)
 print(app.get_result_equation())
